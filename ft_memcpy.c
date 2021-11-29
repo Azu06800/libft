a9d2365fb08164c5a0612c93b9f419303897ccc8
Nizar Hamdan <nizarhamdan@MacBook-Air-de-Nizar.local>
2021-11-29 17:00:14 +0100

    test

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhamdan <nhamdan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 07:38:28 by nhamdan           #+#    #+#             */
+/*   Updated: 2021/11/28 07:47:42 by nhamdan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*t1;
+	unsigned char	*t2;
+
+	t1 = (unsigned char *)src;
+	t2 = (unsigned char *)dst;
+	while (n > 0)
+	{
+		*t2++ = *t1++;
+		n--;
+	}
+	return (dst);
+}
